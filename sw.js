f7f4630b4346f3ce83b056f4187fd3be10ec227c
velwot <velwott@gmail.com>
2026-09-05 23:52:55 +0530

    v4.1

--- sw.js
+++ sw.js
@@ -1,14 +1,1 @@
-const CACHE="ict-career-v3";
-const ASSETS=["./","./index.html","./data/public-template.json","./manifest.webmanifest"];
-self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
-});
-self.addEventListener("activate",e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
-});
-self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET") return;
-  e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{
-    const clone=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,clone)); return res;
-  }).catch(()=>caches.match("./index.html"))));
-});
+const CACHE="ict-career-v4";const ASSETS=["./","./index.html","./data/public-template.json","./manifest.webmanifest"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(h=>h||fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match("./index.html"))))});
